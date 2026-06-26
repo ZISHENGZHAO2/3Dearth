a496b9638772c1b7aa26e26820131a1e939c613d
ZISHENGZHAO2 <1839542565@qq.com>
2026-06-26 17:58:52 +0800

    删除临时文件do_pull.py

--- .github/scripts/do_pull.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import subprocess, sys
-r = subprocess.run(['git', 'pull'], cwd=sys.argv[1] if len(sys.argv)>1 else '.', capture_output=True, text=True)
-print(r.stdout)
-if r.stderr: print('STDERR:', r.stderr[:500])
-print('Exit:', r.returncode)
